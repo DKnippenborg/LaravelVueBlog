5c88d66956af70f9085b9b030468826d8253594c
DKnippenborg <Knippenborg_d@hotmail.com>
2019-11-04 19:03:18 +0100

    updated some scaffolding

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+<!-- replace containt with vue-router when lesson completed at laracasts -->
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
